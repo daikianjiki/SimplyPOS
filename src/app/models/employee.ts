853be84a3cb9c5a6ce550f99cc9e2fe1ee43f77c
Daiki Anjiki <daikianjiki@gmail.com>
2024-04-01 17:24:28 -0400

    created models/employee interface

---
+++ src/app/models/employee.ts
@@ -0,0 +1,28 @@
+/**
+ * In Angular, model interfaces are TS interfaces that define the structure and shape of the data
+ * objects used within an application. These interfaces serve as contracts that define the properties
+ * and their types that a particular data object should have.
+ * 
+ * Model interfaces play a crucial role in maintaining strong typing and providing a clear definition
+ * of the data structures used throughout an Angular application. They help in improving code
+ * readability, facilitating code navigation, and enabling early detection of potential errors.
+ * 
+ * Interfaces in TS enable static type checking. By defining interfaces for data objects,
+ * you can leverage the type system to catch errors at compile-time, helping to prevent 
+ * runtime issues and improve the reliability of your application. 
+ */
+
+export enum AccessLevel {
+    ASSOCIATE = 'associate', //associate has limited access to complete a sales-related transactions without access to inventory transactions
+    STOCK = 'stock', //stock has limited access to complete inventory-related transactions without access to ales transactions
+    MANAGER = 'manager' //manager has complete access to both sales and inventory-related transactions
+}
+
+export interface Employee {
+    id: number //required contraint of a data object
+    username: number //username for POS is typically numberical
+    password: string | number 
+    firstName: string
+    lastName: string
+    accessLevel: AccessLevel //this will define the access level for each employee
+}
