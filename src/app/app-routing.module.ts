3b14a338ce0e3f094aa42cc75d9a8ff28b8e2cb9
Daiki Anjiki <daikianjiki@gmail.com>
2024-04-15 19:56:03 -0400

    updated the router with all the paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.component';
+import { EmployeePageComponent } from './pages/employee-page/employee-page.component';
+import { InventoryPageComponent } from './pages/inventory-page/inventory-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { TransactionPageComponent } from './pages/transaction-page/transaction-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardPageComponent },
+  { path: 'employee', component: EmployeePageComponent },
+  { path: 'inventory', component: InventoryPageComponent },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'transaction', component: TransactionPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
